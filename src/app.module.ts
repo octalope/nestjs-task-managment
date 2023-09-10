3d0bfc92439926cc6f52ebbb7b8211b0cd37d9f1
Steve Brooks <sbrooks@accusoft.com>
2023-09-10 16:07:16 -0400

    add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,48 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { AuthModule } from './auth/auth.module';
 import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     AuthModule,
-    LoggerModule.forRoot({
-      pinoHttp: {
-        level: process.env.LOG_LEVEL, // trace | debug | info | warn | error | fatal
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          pinoHttp: {
+            level: configService.get('LOG_LEVEL'), // trace | debug | info | warn | error | fatal
+          }
+        };
       },
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'taskuser',
-      password: '34Ta5kUs3r!',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        console.log(`.env.stage.${process.env.STAGE}`);
+
+        const result: TypeOrmModuleOptions = {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+        console.log(result);
+        return result;
+      },
     }),
   ],
 })
